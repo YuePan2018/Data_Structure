2ffa4ac164af9472b99ab6db441d708fc0ee12c0
Yue Pan <ypan13@binghamton.edu>
2018-12-27 19:55:45 -0500

    lab1 LeapYear add comment

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,7 +3,7 @@
 
 public class LeapYear{
 	public static void main(String[] args){
-		int year = Integer.parseInt(args[0]);
+		int year = Integer.parseInt(args[0]);				// convert String to Int
 		if(((year%400)==0)||((year%4==0)&&(year%100!=0))){
 			System.out.println(args[0]+" is a leap year.");
 		}
